dba781e708eedcde805c77eda61ddc8906dde2ff
ejs7460 <46607985+ejs7460@users.noreply.github.com>
2020-03-17 17:05:41 -0400

    added feedforward function for neural net

--- layer.py
+++ layer.py
@@ -2,7 +2,7 @@ from activations import *
 import numpy as np
 
 class Layer():
-    def __init__(self, input_dim, output_dim, activation):
+    def __init__(self, input_dim=0, output_dim, activation):
         """
         Constructor
         
@@ -10,10 +10,13 @@ class Layer():
         output_dim : number of outputs for the layer, essentially the number of neurons in the layer
         
         """
+        self.input_dim = input_dim
+        self.output_dim = output_dim
         self.activations = np.array([])
         self.activation = activation
         self.weights = np.random.rand(output_dim, input_dim)
         self.biases = np.random.rand(output_dim)
+        
         
     def feedforward(self, input_layer):
         input_activations = input_layer.activations
@@ -22,3 +25,8 @@ class Layer():
         
         if self.activation is 'sig':
             self.activations = np.vectorize(sigmoid)(activations)
+            
+            
+    def update_dim(input_dim):
+        self.input_dim = input_dim
+        self.weights = np.random.rand(self.output_dim, self.input_dim)
--- neural_net.py
+++ neural_net.py
@@ -3,4 +3,20 @@ class NeuralNetwork():
         self.layers = []
         
     def add_layer(self, layer):
+        if len(self.layers) > 0:
+            input_dim = layers[len(layers)-1].output_dim
+            layer.update(input_dim)
+            
         self.layers.append(layer)
+    
+        
+    def feedforward(inputs):
+        self.layers[0].activations = inputs
+        for i in range(1, len(self.layers)):
+            layer_i = layers[i]
+            layer_i.feedforward(self.layers[i-1])
+        
+        output = self.layers[len(self.layers)].activations
+        
+        return output # return the output after the inputs feedforward throught the netowrk
+    
