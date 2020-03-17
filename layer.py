9d15e5da486acfeaa68aef8380740a3893e62622
ejs7460 <46607985+ejs7460@users.noreply.github.com>
2020-03-17 16:42:47 -0400

    removed Neuron class. Using basic matrix algebra in layer class

--- layer.py
+++ layer.py
@@ -1,7 +1,8 @@
-from neuron import Neuron
+from activations import *
+import numpy as np
 
 class Layer():
-    def __init__(self, input_dim, output_dim):
+    def __init__(self, input_dim, output_dim, activation):
         """
         Constructor
         
@@ -9,4 +10,24 @@ class Layer():
         output_dim : number of outputs for the layer, essentially the number of neurons in the layer
         
         """
-        self.neurons = [Neuron() for i in range(0, output_dim)]
+        self.activations = np.array([])
+        self.activation = activation
+        self.weights = np.random.rand(output_dim, input_dim)
+        self.biases = np.random.rand(output_dim)
+        
+    def feedforward(self, input_layer):
+        input_activations = input_layer.activations
+        
+        activations = np.add(np.dot(self.weights, input_activations), self.biases)
+        
+        if self.activation is 'sig':
+            self.activations = np.vectorize(sigmoid)(activations)
+        
+        
+L = Layer(4, 2, 'sig')
+input_L = Layer(8, 4, 'sig')
+input_L.activations = np.random.rand(4)
+
+L.feedforward(input_L)
+
+print(L.activations)
---
+++ neural_net.py
@@ -0,0 +1,6 @@
+class NeuralNetwork():
+    def __init__(self):
+        self.layers = []
+        
+    def add_layer(self, layer):
+        self.layers.append(layer)
