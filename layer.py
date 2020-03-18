29335cb71e91fc7ac7c31a5bf40205cf17066c61
ejs7460 <46607985+ejs7460@users.noreply.github.com>
2020-03-18 00:51:40 -0400

    got working NeuralNetwork feedfoward

--- layer.py
+++ layer.py
@@ -2,7 +2,7 @@ from activations import *
 import numpy as np
 
 class Layer():
-    def __init__(self, input_dim=0, output_dim, activation):
+    def __init__(self, output_dim, activation="sig"):
         """
         Constructor
         
@@ -10,23 +10,27 @@ class Layer():
         output_dim : number of outputs for the layer, essentially the number of neurons in the layer
         
         """
-        self.input_dim = input_dim
+        self.input_dim = 0
         self.output_dim = output_dim
         self.activations = np.array([])
         self.activation = activation
-        self.weights = np.random.rand(output_dim, input_dim)
+        self.weights = np.random.rand(self.output_dim, self.input_dim)
         self.biases = np.random.rand(output_dim)
+        
+    def __str__(self):
+        return f"Layer -> input_dim : {self.input_dim}, output_dim : {self.output_dim}"
         
         
     def feedforward(self, input_layer):
         input_activations = input_layer.activations
         
-        activations = np.add(np.dot(self.weights, input_activations), self.biases)
+        dot_product = np.dot(self.weights, input_activations)
+        activations = np.add(dot_product, self.biases)
         
         if self.activation is 'sig':
             self.activations = np.vectorize(sigmoid)(activations)
             
             
-    def update_dim(input_dim):
+    def update_dim(self, input_dim):
         self.input_dim = input_dim
         self.weights = np.random.rand(self.output_dim, self.input_dim)
--- neural_net.py
+++ neural_net.py
@@ -1,22 +1,43 @@
+import numpy as np
+from layer import Layer
+
 class NeuralNetwork():
     def __init__(self):
         self.layers = []
         
     def add_layer(self, layer):
+        if len(self.layers) == 0:
+            input_dim = layer.output_dim
+            layer.update_dim(input_dim)
+
         if len(self.layers) > 0:
-            input_dim = layers[len(layers)-1].output_dim
-            layer.update(input_dim)
+            input_dim = self.layers[len(self.layers)-1].output_dim
+            layer.update_dim(input_dim)
             
         self.layers.append(layer)
+        
+        
+    def calc_error(self):
+        pass
     
         
-    def feedforward(inputs):
+    def feedforward(self, inputs):
         self.layers[0].activations = inputs
         for i in range(1, len(self.layers)):
-            layer_i = layers[i]
+            layer_i = self.layers[i]
             layer_i.feedforward(self.layers[i-1])
         
-        output = self.layers[len(self.layers)].activations
+        output = self.layers[len(self.layers)-1].activations
         
         return output # return the output after the inputs feedforward throught the netowrk
     
+
+# N = NeuralNetwork()
+# N.add_layer(Layer(16))
+# N.add_layer(Layer(4))
+# N.add_layer(Layer(2))
+
+# output = N.feedforward(np.random.rand(16))
+
+# print(output)
+
