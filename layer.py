b184c9002e931b5996c452dc4c307aac396954f6
ejs7460 <46607985+ejs7460@users.noreply.github.com>
2020-03-20 19:12:22 -0400

    solved vanishing/exploding gradient problem with careful initialization of weights, however it still doesnt' fit properly

--- layer.py
+++ layer.py
@@ -14,11 +14,16 @@ class Layer():
         self.output_dim = output_dim
         self.activations = np.array([])
         self.activation = activation
-        self.weights = np.random.rand(self.output_dim, self.input_dim)
-        self.biases = np.random.rand(output_dim)
+        self.weights = np.random.rand(self.output_dim, self.input_dim)/1000#(self.input_dim + self.output_dim)
+        self.biases = np.random.rand(output_dim)/100#(self.input_dim + self.output_dim)
+        
         
     def __str__(self):
         return f"Layer -> input_dim : {self.input_dim}, output_dim : {self.output_dim}"
+    
+    
+    def update_weights(self, adjustments):
+        self.weights = self.weights - adjustments
         
         
     def feedforward(self, input_layer):
@@ -26,11 +31,26 @@ class Layer():
         
         dot_product = np.dot(self.weights, input_activations)
         activations = np.add(dot_product, self.biases)
+        # print(activations)
         
         if self.activation is 'sig':
             self.activations = np.vectorize(sigmoid)(activations)
+            # print(self.activations)
+        elif self.activation is 'none':
+            self.activations = activations
             
             
     def update_dim(self, input_dim):
         self.input_dim = input_dim
-        self.weights = np.random.rand(self.output_dim, self.input_dim)
+        self.weights = np.random.rand(self.output_dim, self.input_dim)/1000#(self.input_dim + self.output_dim)
+
+
+class InputLayer(Layer):
+    def __init__(self, input_dim):
+        self.input_dim = input_dim
+        self.output_dim = input_dim
+        self.activations = np.array([])
+        
+        
+    def __str__(self):
+        return f"InputLayer -> input_dim : {self.input_dim}"
--- neural_net.py
+++ neural_net.py
@@ -1,15 +1,13 @@
 import numpy as np
-from layer import Layer
+from layer import Layer, InputLayer
+from sklearn.datasets import fetch_openml
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 
 class NeuralNetwork():
     def __init__(self):
         self.layers = []
         
     def add_layer(self, layer):
-        if len(self.layers) == 0:
-            input_dim = layer.output_dim
-            layer.update_dim(input_dim)
-
         if len(self.layers) > 0:
             input_dim = self.layers[len(self.layers)-1].output_dim
             layer.update_dim(input_dim)
@@ -17,14 +15,15 @@ class NeuralNetwork():
         self.layers.append(layer)
         
         
-    def total_error(self, expected, predicted):
+    def error(self, expected, predicted):
         error = np.subtract(expected, predicted)
         squared = .5 * np.square(error)
-        total_error = np.sum(squared)
-        return total_error
+        error = np.sum(squared)
+        return error
         
         
     def feedforward(self, inputs):
+        # print("feedforward")
         self.layers[0].activations = inputs
         for i in range(1, len(self.layers)):
             layer_i = self.layers[i]
@@ -40,22 +39,91 @@ class NeuralNetwork():
     
     
     def delta_out_delta_net(self, output):
-        return np.multiply(np.subtract(1, output))
+        return np.multiply(output, np.subtract(1, output))
     
     
-    def delta_net_delta_weights(self, index):
-        return layers[i].activations
+    def delta_net_delta_weights(self, i):
+        return self.layers[i-1].activations
     
     
-    def backprop(self, expected, predicted, index):
-        pass
+    def backprop(self, expected):
+        for i in range(len(self.layers)-1, 0, -1):
+            layer_i = self.layers[i]
+            predicted_i = layer_i.activations
+            # print(predicted_i)
+            delta_error_delta_out = self.delta_error_delta_out(expected, predicted_i)
+            # print(delta_error_delta_out)
+            delta_out_delta_net = self.delta_out_delta_net(predicted_i)
+            # print(delta_out_delta_net)
+            delta_net_delta_weights = self.delta_net_delta_weights(i)
             
+            a = delta_error_delta_out * delta_out_delta_net
+            # print(a)
+            
+            a_prime = a.reshape((-1, 1))
+            
+            b = delta_net_delta_weights
+            
+            c = a_prime * b
+    
+            layer_i.update_weights(c)
+            
+            
+    def fit(self, data, labels, epochs=1):
+        print("Training...")
+        for epoch in range(epochs):
+            i = 0
+            while i < len(data):
+                data_i = data[i]
+                label_i = labels[i]
+                self.feedforward(data_i)
+                self.backprop(label_i)
+                i+=1
+            print("Epoch " + str(epoch))
+        print("Finished training!")
+        
+    
+    def predict(self, x):
+        return self.feedforward(x)
+    
+    
+    def evaluate(self, X, y):
+        correct = 0
+        print("Evaluating...")
+        for i in range(0, len(X)):
+            predicted = self.predict(X[i])
+            error = self.error(y[i], predicted)
+            if error < .05:
+                count += 1
+        print("Finished evaluating!")
+        return correct
+            
+le = LabelEncoder()
+ohe = OneHotEncoder(sparse=False)
 
-# N = NeuralNetwork()
-# N.add_layer(Layer(16))
-# N.add_layer(Layer(4))
-# N.add_layer(Layer(2))
+mnist = fetch_openml('mnist_784', version=1)
+X, y = mnist["data"], mnist["target"]
 
-# output = N.feedforward(np.random.rand(16))
+y = ohe.fit_transform(y.reshape((-1, 1)))
 
-# print(output)
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+X_train = np.array(X_train)
+X_test = np.array(X_test)
+y_train = np.array(y_train)
+y_test = np.array(y_test)
+
+N = NeuralNetwork()
+N.add_layer(InputLayer(784))
+N.add_layer(Layer(10, activation='sig'))
+
+N.fit(X_train, y_train, epochs=10)
+
+for i in range(0, 100):
+    predicted = N.predict(X_test[i])
+    print(np.round(predicted))
+    # print(y_test[i])
+print("Finished evaluating!")
+
+# print(N.evaluate(X_test, y_test))
+
+
